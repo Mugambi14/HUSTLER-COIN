689363f28a26ff875237c25dfbd7d7247da60db5
Mugambi14 <lewismhunga@gmail.com>
2026-05-09 11:41:15 +0300

    Critical: Updated Seed Node IP to 102.209.76.210 and enabled P2Pool

--- p2p-node.js
+++ p2p-node.js
@@ -15,8 +15,8 @@ const color = {
 // --- CONFIGURATION ---
 const HTTP_PORT = process.env.HTTP_PORT || 3000;
 const P2P_PORT = process.env.P2P_PORT || 5001;
-const initialPeers = ['ws://197.248.102.155:5001'];
-const GLOBAL_POOL_URL = 'http://197.248.102.155:3000/pool/submit';
+const initialPeers = ['ws://102.209.76.210:5001'];
+const GLOBAL_POOL_URL = 'http://102.209.76.210:3000/pool/submit';
 
 const HustlerCoin = new Blockchain();
 HustlerCoin.loadChain(); 
